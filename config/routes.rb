18af2bd8d10bf83cb104c55df2bcbd2d8336220b
Paulina Paetzold <paulinapaetzold@gmx.de>
2020-11-17 20:47:59 +0100

    finished ex.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CREATE
+  get 'article/new', to: 'articles#new', as: :new
+  post 'articles', to: 'articles#create'
+
+  # READ
+  get 'articles', to: 'articles#index'
+  get 'article/:id', to: 'articles#show', as: :article
+
+  # UPDATE
+  get 'article/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'article/:id', to: 'articles#update'
+
+  # DELETE
+  delete 'article/:id', to: 'articles#destroy'
 end
